6a7cbaf4a873558bf4d685fff16c6f802286f4a1
Carlos <carlos.estradaih@gmail.com>
2020-11-21 22:51:56 -0700

    added dashboard with portfolio chart and Total value of investments

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import clsx from 'clsx';
+import useStyles from './DashboardStyles.js';
+import {CssBaseline, Drawer, Box, AppBar, Toolbar, List, Typography, Divider, IconButton, Badge, Container, Grid, Paper, Link} from '@material-ui/core';
+import {MenuOutlined, ChevronLeft} from '@material-ui/icons';
+import {glistItems} from './ListItems/listItems';
+import InvestmentsG from './InvestmentsG/InvestmentsG'
+import PGraph from '../Portfolio/PGraph/PGraph';
+
+
+export default function Dashboard() {
+    const classes = useStyles();
+    const [extend, setExtend] = React.useState(true);
+    const handleDrawerExtend = () => {
+        setExtend(true);
+    };
+    const handleDrawerShrink = () => {
+        setExtend(false);
+    };
+    //combination of both styles
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+    return (
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar position="absolute" className={clsx(classes.appBar, extend && classes.appBarShift)}>
+            <Toolbar className={classes.toolbar}>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerExtend}
+                className={clsx(classes.menuButton, extend && classes.menuButtonHidden)}
+              >
+                <MenuOutlined />
+              </IconButton>
+              <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                Dashboard
+              </Typography>
+              <IconButton color="inherit">
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: clsx(classes.drawerPaper, !extend && classes.drawerPaperClose),
+            }}
+            extend={extend}
+          >
+            <div className={classes.toolbarIcon}>
+              <IconButton onClick={handleDrawerShrink}>
+                <ChevronLeft />
+              </IconButton>
+            </div>
+            <Divider />
+            <List>{glistItems}</List>
+            <Divider />
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="lg" className={classes.container}>
+              <Grid container spacing={3}>
+                {/* Chart */}
+                <Grid item xs={12} md={8} lg={9}>
+                  <Paper className={fixedHeightPaper}>
+                    <PGraph />
+                  </Paper>
+                </Grid>
+                {/* Recent Deposits */}
+                <Grid item xs={12} md={4} lg={3}>
+                  <Paper className={fixedHeightPaper}>
+                    <InvestmentsG />
+                  </Paper>
+                </Grid>
+                {/* Recent Orders */}
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                  </Paper>
+                </Grid>
+              </Grid>
+              <Box pt={4}>
+              </Box>
+            </Container>
+          </main>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6d7188',
+      main: '#494E6B',
+      dark: '#33364a',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ac7e8a',
+      main: '#985E6D',
+      dark: '#6a414c',
+    }
+  },
+  status: {
+    danger: 'orange',
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MuiThemeProvider theme ={theme}>
+      <App />
+    </MuiThemeProvider>,
   document.getElementById('root')
 );
 
