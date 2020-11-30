d5b094afa311b3fb33c2add51b87e2c5a0602d3f
JesusSierra <jsierraher@gmail.com>
2020-11-29 21:20:52 -0700

    registration complete

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,12 @@
-class RegistrationsController < Applicationscontroller
+class RegistrationsController < ApplicationController
     def create
         user = User.create!(
             email: params['user']['email'],
-            password: params['user']['password'],
-            password_confirmation: params['user']['password_confirmation']
+            password: params['user']['password']
         )
         if user
             session[:user_id] = user.id
-            render json {
+            render json: {
                 status: :created,
                 user: user
             }
