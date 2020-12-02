097394fc7d871b626d654a9e623d170a681a4101
JesusSierra <jsierraher@gmail.com>
2020-12-01 18:49:37 -0700

    No funciona

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Auth from './Components/Auth/Auth'
 import useStyles from '../src/AppStyles';
 import Dashboard from './Components/Dashboard/Dashboard'
 import {BrowserRouter, Switch, Route} from "react-router-dom";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import { data } from 'jquery';
 import Budget from './Components/Budget/Budget'
 import Portfolio from './Components/Portfolio/Portfolio';
@@ -17,6 +18,33 @@ function App(props) {
       setloggedInStatus("LOGGED_IN");
       setuser(data);
     }
+
+    /* const checkLoginStatus = () => {
+      axios
+      .get("http://localhost:3001/logged_in", {withCredentials: true }).then(response => {
+        if (response.data.logged_in && loggedInStatus === "NOT_LOGGED_IN"){
+          setloggedInStatus({
+            loggedInStatus: "LOGGED_IN"
+          })
+          setuser({
+            user : response.data.user
+          })
+        }else if(!response.data.logged_in && loggedInStatus === "LOGGED_IN"){
+          setloggedInStatus({
+            loggedInStatus: "NOT_LOGGED_IN"
+          })
+          setuser({
+            user : {}
+          })
+        }
+      }).catch(error => {
+        console.log("check login error", error);
+      });
+    } */
+
+    /* useEffect(() => {
+      checkLoginStatus()
+    },[]);  */
   
     //CHANGE ROUTE TO PROTECTEDROUTE TO PROTECT :V
   return (
