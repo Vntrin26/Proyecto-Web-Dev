0fc73797dea1e5af7a7d7b2fd45d00fd8737d6cf
Vntrin26 <31109455+Vntrin26@users.noreply.github.com>
2020-12-03 14:43:22 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     
     allow do
     origins "https://money-desk.herokuapp.com"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
